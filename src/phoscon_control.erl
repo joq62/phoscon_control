e310592f131b39541dfe138c9a5b98c1894983f5
joq62 <jakim.leche@gmail.com>
2023-09-11 00:33:09 +0200

    corr

--- src/phoscon_control.erl
+++ src/phoscon_control.erl
@@ -135,7 +135,7 @@ init([]) ->
     {ok,HostName}=net:gethostname(),
 %    {ok,[{conbee,ConbeeConfig}]}=etcd_host:get_appl_config(HostName),
 
-    {ok,[{conbee,ConbeeConfig}]}=sd:call(etcd_provider,etcd_host,get_appl_config,[HostName],5000),
+    {ok,[{conbee,ConbeeConfig}]}=sd:call(etcd,etcd_host,get_appl_config,[HostName],5000),
     {conbee_addr,ConbeeAddr}=lists:keyfind(conbee_addr,1,ConbeeConfig),
     {conbee_port,ConbeePort}=lists:keyfind(conbee_port,1,ConbeeConfig),
     {conbee_key,ConbeeKey}=lists:keyfind(conbee_key,1,ConbeeConfig),
